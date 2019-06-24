fdb416e656f09098f555c08174a379ea9d48e288
Vasiliy Vasilyuk <xorcare@gmail.com>
2019-06-25 00:14:08 +0300

    Make startup tools installer for macOS on brew

---
+++ brew.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install Bash 4.
+brew install bash
+brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
+
+# Install GnuPG to enable PGP-signing commits.
+brew install gnupg
+
+# Install font tools.
+brew tap bramstein/webfonttools
+brew install sfnt2woff \
+    sfnt2woff-zopfli \
+    woff2
+brew tap homebrew/cask-fonts
+brew cask install \
+    font-fira-code \
+    font-fira-mono \
+    font-fira-mono-for-powerline \
+    font-fira-sans
+
+# Install other useful binaries.
+brew install git
+brew install tig
+brew install git-lfs
+brew install htop
+brew install wget
+brew install tarsnap
+brew install transmission
+
+brew cask install jetbrains-toolbox
+
+# Remove outdated versions from the cellar.
+brew cleanup
