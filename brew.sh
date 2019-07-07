f4b013529de9275d16d3fa64dd50aefa34a9980f
Vasiliy Vasilyuk <xorcare@gmail.com>
2019-07-07 15:55:16 +0300

    Fix the overwrite command
    Not to overwrite existing commands sha256sum
    if one already exists.

--- brew.sh
+++ brew.sh
@@ -14,7 +14,7 @@ BREW_PREFIX=$(brew --prefix)
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
-ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+[ ! -x "$(command -v 'sha256sum')" ] && ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 
 # Install some other useful utilities like `sponge`.
 brew install moreutils
