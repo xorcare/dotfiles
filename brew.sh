820a2d1bf9e0113a0485f16e3d717abc694875e8
Vasiliy Vasilyuk <xorcare@gmail.com>
2020-04-04 00:45:48 +0300

    Do not automatically change the default interpreter
    I do not want to use this behavior because I want to switch to zsh.

--- brew.sh
+++ brew.sh
@@ -24,12 +24,6 @@ brew install findutils
 brew install bash
 brew install bash-completion2
 
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-  chsh -s "${BREW_PREFIX}/bin/bash";
-fi;
-
 # Install GnuPG to enable PGP-signing commits.
 brew install gnupg
 
