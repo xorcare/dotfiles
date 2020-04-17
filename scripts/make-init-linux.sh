0c6e3e08e253a1780835f464f40f46261543ed03
Vasiliy Vasilyuk <xorcare@gmail.com>
2020-04-17 14:56:22 +0300

    Add translation installation

--- scripts/make-init-linux.sh
+++ scripts/make-init-linux.sh
@@ -5,4 +5,7 @@
 [ -x "$(command -v 'apt')" ] && $SHELL "$DOTFILES_ROOT/scripts/make-init-linux-apt.sh" && exit $?
 [ -x "$(command -v 'yum')" ] && $SHELL "$DOTFILES_ROOT/scripts/make-init-linux-yum.sh" && exit $?
 
+sudo locale-gen en_US.UTF-8
+sudo locale-gen ru_RU.UTF-8
+
 echo "Nothing to do!"
