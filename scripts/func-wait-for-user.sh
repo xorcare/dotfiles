206aa7ff1da0bb96a3302ef25f46f6e1e1861844
Vasiliy Vasilyuk <xorcare@gmail.com>
2020-07-31 11:31:19 +0300

    Add github actions CI
    This is necessary to check the performance of the scripts to be more confident in their performance.

--- scripts/func-wait-for-user.sh
+++ scripts/func-wait-for-user.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+readonly DOTFILES_ROOT CI
+
 [[ -z "$DOTFILES_ROOT" ]] && echo 'Please set a variable $DOTFILES_ROOT or use Makefile!' && exit 1
 
 source "$DOTFILES_ROOT/scripts/func-getc.sh"
 
 wait_for_user() {
+	if ! [ -z "${CI}" ] ; then return ; fi
 	local c
 	echo
 	echo "Press RETURN to continue or any other key to abort"
