86605f0a789df7b9e3ba9709a9089ef7c33d9d0b
Vasiliy Vasilyuk <xorcare@gmail.com>
2021-05-31 21:33:46 +0300

    Redo the installation functions
    Reworked the installation process and described the documentation for
    the new commands. Among the old teams, not all are fully working and
    because of this they give false hope that some problems will be fixed.
    For now, I have left only the installation functions that are regularly
    checked on CI and should work stably.
    
    Since logging changes interfered with the normal operation of ci
    checks, logging was completely removed.

--- scripts/make-install-software-linux-apt.sh
+++ scripts/make-install-software-linux-apt.sh
@@ -8,6 +8,8 @@ source "$DOTFILES_ROOT/scripts/func-wait-for-user.sh"
 echo "You want to start the installation of tools with apt. Are you sure?"
 wait_for_user
 
+set -x
+
 sudo apt-get update
 
 sudo apt install xclip -y
@@ -21,9 +23,11 @@ sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt update
 sudo apt install code -y
-sudo apt install shellcheck
+sudo apt install shellcheck -y
 # Lightweight and flexible command-line JSON processor.
 # NOTE: it's need for working scripts from home/bin.
-sudo apt install jq
+sudo apt install jq -y
 
 sudo apt autoremove
+
+exit 0
--- scripts/make-install-software-linux-yum.sh
+++ scripts/make-install-software-linux-yum.sh
@@ -8,6 +8,8 @@ source "$DOTFILES_ROOT/scripts/func-wait-for-user.sh"
 echo "You want to start the installation of tools with yum. Are you sure?"
 wait_for_user
 
+set -x
+
 sudo yum check-update
 
 sudo yum install xclip -y
@@ -23,6 +25,8 @@ sudo yum install code -y
 sudo yum install shellcheck
 # Lightweight and flexible command-line JSON processor.
 # NOTE: it's need for working scripts from home/bin.
-sudo yum install jq
+sudo yum install jq -y
 
 sudo yum autoremove
+
+exit 0
