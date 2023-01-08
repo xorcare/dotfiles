393d33b221ac0800c179a405bb29336b28e34f3f
Vasiliy Vasilyuk <xorcare@gmail.com>
2023-01-08 22:45:38 +0300

    Change screenshot file name prefix to `Screenshot`

--- scripts/make-defaults.sh
+++ scripts/make-defaults.sh
@@ -212,6 +212,9 @@ defaults write com.apple.screencapture type -string "png"
 # Disable shadow in screenshots
 defaults write com.apple.screencapture disable-shadow -bool true
 
+# Change screenshot file name prefix to "Screenshot"
+defaults write com.apple.screencapture name "Screenshot"
+
 # Enable subpixel font rendering on non-Apple LCDs
 # Reference: https://github.com/kevinSuttle/macOS-Defaults/issues/17#issuecomment-266633501
 defaults write NSGlobalDomain AppleFontSmoothing -int 1
